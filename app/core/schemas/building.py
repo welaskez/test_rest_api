d1ee64ec0c9415504350a5cbf3c3738247b26aed
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-15 02:52:31 +0500

    feat: create building schemas

---
+++ app/core/schemas/building.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class BuildingCreate(BaseModel):
+    county: str
+    region: str
+    city: str
+    district: str | None = None
+    micro_district_or_street: str
+    number: str
+    postal_code: int
+    latitude: float
+    longitude: float
+
+
+class BuildingRead(BuildingCreate):
+    pass
+
+
+class BuildingUpdate(BaseModel):
+    county: str | None = None
+    region: str | None = None
+    city: str | None = None
+    district: str | None = None
+    micro_district_or_street: str | None = None
+    number: str | None = None
+    postal_code: int | None = None
+    latitude: float | None = None
+    longitude: float | None = None
