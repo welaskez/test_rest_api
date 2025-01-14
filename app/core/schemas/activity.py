8a2cde928cd9b9367526fc1ab3ac37ade88102ea
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-15 02:52:40 +0500

    feat: create activity schemas

---
+++ app/core/schemas/activity.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class ActivityCreate(BaseModel):
+    name: str
+    parent_id: UUID | str
+
+
+class ActivityRead(BaseModel):
+    name: str
+    parent: "ActivityRead"
+    childrens: list["ActivityRead"]
+
+
+class ActivityUpdate(BaseModel):
+    name: str | None = None
+    parent_id: UUID | str | None = None
