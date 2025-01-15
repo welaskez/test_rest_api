28e66bcffbd9eca3db4123fd107588583280f7cb
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-15 14:52:09 +0500

    feat: create building router, added router to v1 router & implement getting organizations by building endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,4 +2,7 @@ __all__ = ("router",)
 
 from fastapi import APIRouter
 
+from .buildings import router as buildings_router
+
 router = APIRouter(prefix="/v1")
+router.include_router(buildings_router)
---
+++ app/api/v1/buildings.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+from uuid import UUID
+
+from core.schemas.organization import OrganizationRead
+from fastapi import APIRouter, Depends, Path, status
+from services.building import BuildingService
+
+from api.dependencies.services import get_building_service
+
+router = APIRouter(prefix="/buildings", tags=["Buildings"])
+
+
+@router.get(
+    path="/{building_id}/organizations",
+    status_code=status.HTTP_200_OK,
+    response_model=list[OrganizationRead],
+)
+async def get_organizations_in_building(
+    building_service: Annotated[BuildingService, Depends(get_building_service)],
+    building_id: Annotated[UUID, Path(description="ID of building")],
+):
+    """Return organizations in building by building id"""
+    return await building_service.get_organizations_by_building_id(building_id)
