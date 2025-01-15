0364bfabb2f14973562bc68f098bf2ae827c37d4
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-15 15:07:33 +0500

    feat: added getting activity service dep

--- app/api/dependencies/services.py
+++ app/api/dependencies/services.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from core.models import db_helper
 from fastapi import Depends
+from services.activity import ActivityService
 from services.building import BuildingService
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -10,3 +11,9 @@ def get_building_service(
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
 ) -> BuildingService:
     return BuildingService(session)
+
+
+def get_activity_service(
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+) -> ActivityService:
+    return ActivityService(session)
