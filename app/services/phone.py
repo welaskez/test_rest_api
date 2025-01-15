9529f07259dd278a8f9c8621dbd3f6c461449cab
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-16 02:28:00 +0500

    refactor: remove serialize phone method

--- app/services/phone.py
+++ app/services/phone.py
@@ -1,5 +1,4 @@
 from core.models import Phone
-from core.schemas.phone_number import PhoneRead
 from crud.phone import PhoneCRUD
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -10,12 +9,3 @@ class PhoneService(BaseService[Phone]):
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session, PhoneCRUD)
         self.crud: PhoneCRUD = self.crud
-
-
-def serialize_phone(phone: Phone) -> PhoneRead:
-    """
-    Serialize SQLA model to pydantic schema
-    :param phone: SQLA model of phone
-    :return: phone read pydantic schema
-    """
-    return PhoneRead(number=phone.number)
