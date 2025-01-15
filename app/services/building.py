95a0455639aa32dc30541552752d00a946d5f164
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-15 15:06:25 +0500

    feat: create serialize building method

--- app/services/building.py
+++ app/services/building.py
@@ -1,13 +1,13 @@
 from uuid import UUID
 
 from core.models import Building
+from core.schemas.building import BuildingRead
 from core.schemas.organization import OrganizationRead
 from crud.building import BuildingCRUD
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import BaseService
-from .organization import serialize_organization
 
 
 class BuildingService(BaseService[Building]):
@@ -18,6 +18,13 @@ class BuildingService(BaseService[Building]):
     async def get_organizations_by_building_id(
         self, building_id: UUID
     ) -> list[OrganizationRead]:
+        """
+        Getting organizations in building
+        :param building_id: building ID
+        :return: list of organizations
+        """
+        from .organization import serialize_organization
+
         organizations = await self.crud.get_organizations_by_building(building_id)
 
         if organizations:
@@ -29,3 +36,22 @@ class BuildingService(BaseService[Building]):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No organizations in this building!",
         )
+
+
+def serialize_building(building: Building) -> BuildingRead:
+    """
+    Serialize SQLA model to pydantic schema
+    :param building: SQLA model of building
+    :return: building read pydantic schema
+    """
+    return BuildingRead(
+        county=building.county,
+        region=building.region,
+        city=building.city,
+        district=building.district,
+        micro_district_or_street=building.micro_district_or_street,
+        number=building.number,
+        postal_code=building.postal_code,
+        latitude=building.latitude,
+        longitude=building.longitude,
+    )
