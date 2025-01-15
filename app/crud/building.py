da694758e70eb2f289cc828e03fdaecd48f7b955
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-15 14:49:49 +0500

    feat: added method to getting organizations in building in BuildingCRUD

--- app/crud/building.py
+++ app/crud/building.py
@@ -1,5 +1,9 @@
-from core.models import Building
+from uuid import UUID
+
+from core.models import Activity, Building, Organization
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from crud.base import BaseCRUD
 
@@ -9,3 +13,26 @@ class BuildingCRUD(BaseCRUD[Building]):
 
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session)
+
+    async def get_organizations_by_building(
+        self, building_id: UUID
+    ) -> list[Organization]:
+        """
+        Return organizations by building
+        :param building_id: building ID
+        :return: list of organizations
+        """
+        building = await self.session.scalar(
+            select(Building)
+            .options(
+                selectinload(Building.organizations).selectinload(
+                    Organization.building
+                ),
+                selectinload(Building.organizations)
+                .selectinload(Organization.activity)
+                .selectinload(Activity.childrens, recursion_depth=3),
+            )
+            .where(Building.id == building_id)
+        )
+
+        return building.organizations
