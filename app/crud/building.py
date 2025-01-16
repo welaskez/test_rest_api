af0d66e058248f94deb86787e72df5acb799d9a7
welaskez <nazar.nikitenko.03@mail.ru>
2025-01-16 12:50:13 +0500

    refactor: remove unused method

--- app/crud/building.py
+++ app/crud/building.py
@@ -1,9 +1,5 @@
-from uuid import UUID
-
-from core.models import Activity, Building, Organization
-from sqlalchemy import select
+from core.models import Building
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import selectinload
 
 from crud.base import BaseCRUD
 
@@ -13,30 +9,3 @@ class BuildingCRUD(BaseCRUD[Building]):
 
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session)
-
-    async def get_organizations_by_building(
-        self, building_id: UUID
-    ) -> list[Organization]:
-        """
-        Return organizations by building
-        :param building_id: building ID
-        :return: list of organizations
-        """
-        building = await self.session.scalar(
-            select(Building)
-            .options(
-                selectinload(Building.organizations).selectinload(
-                    Organization.building
-                ),
-                selectinload(Building.organizations)
-                .selectinload(Organization.activity)
-                .selectinload(Activity.childrens, recursion_depth=3),
-                selectinload(Building.organizations).selectinload(
-                    Organization.phone_numbers
-                ),
-            )
-            .where(Building.id == building_id)
-        )
-
-        if building:
-            return building.organizations
